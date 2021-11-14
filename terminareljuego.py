9f1ec24afd5068c812b8ba9e9705068ead5b5e85
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-11-14 18:10:34 +0100

    cambios

--- terminareljuego.py
+++ terminareljuego.py
@@ -1,20 +1,43 @@
-def elegir_opcion():
+def elegirOpcion():
     opcion_valida=False
     num=0
+   
     while(not opcion_valida):
         
-        num=int(input("Elige entre 1 y 5"))
-        if(num>=1 or num<=5):
+        num=int(input("Elige entre 1 y 5: "))
+        if(num>=1 and num<=5):
             opcion_valida=True
         else:
-            print("el valor no esta entre el 1 y el 5")
+            print("El valor no esta entre el 1 y el 5")
+            
     return num
 
-print("Menu")
-print("1-Nivel simple")
-print("2-Nivel intermedio")
-print("3-Nivel avanzado")
-print("4-Nivel experto")
-print("5-Salir")
 
-print("Elige una opcion")
+salir=False
+opcion=0
+
+while not salir:
+    print("Menu")
+    print("1-Nivel simple")
+    print("2-Nivel intermedio")
+    print("3-Nivel avanzado")
+    print("4-Nivel experto")
+    print("5-Salir")
+
+    print("Elige una opcion")
+
+    opcion = elegirOpcion()
+
+    print("Opcion elegida ", opcion)
+
+    if opcion==1:
+        print("primera funcion")
+    elif opcion==2:
+        print("segunda funcion")
+    elif opcion==3:
+        print("tercera funcion")
+    elif opcion==4:
+        print("cuarta funcion")
+    elif opcion==5:
+        salir=True
+        print("fin del programa")
